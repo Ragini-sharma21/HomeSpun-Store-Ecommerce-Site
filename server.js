9dc07558ddf02b11831bebdfc1d366c2b85c9cf7
Ragini sharma <88306424+Ragini-sharma21@users.noreply.github.com>
2023-06-03 16:45:53 +0530

    changes ready

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 
 import express from"express";
-import colors from "colors";
 import dotenv from 'dotenv';
 import morgan from "morgan";
 import connectDB from "./config/db.js";
@@ -8,6 +7,7 @@ import authRoutes from './routes/authRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import cors from 'cors'
 import productRoutes from "./routes/productRoutes.js";
+import path from 'path';   //for hosting
 
 //configure env
 dotenv.config(); //our env file is in root so we dont need to mention location 
@@ -23,6 +23,7 @@ const app=express()     //call express
 app.use(cors());
 app.use(express.json()); //call json package
 app.use(morgan("dev"));  //call morgan package 
+app.use(express.static(path.join(__dirname, './client/build')))   //for hosting
 
 
 // routes
@@ -31,10 +32,14 @@ app.use("/api/v1/category",categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get('/',(req,res)=>{  //use arrow function to call api for homepage,we can handle requests and send response to user 
-    res.send("<h1>Welcome to ecommerce app</h1>");     //this is send function
+// app.get('/',(req,res)=>{  //use arrow function to call api for homepage,we can handle requests and send response to user 
+//     res.send("<h1>Welcome to ecommerce app</h1>");     //this is send function
         
-    });
+//     });
+
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"));
+});
 
 //port
 const PORT= process.env.PORT || 8080; //variable to store port address and we are getting the port value from env file by the help of process.env.port or else if port value isnt updated use 8080
